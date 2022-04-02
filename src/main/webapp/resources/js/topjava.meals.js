f419c4e7a97c58b775c909274a8523e2a3da8d89
Dzenovich <zakirov.zrz@gmail.com>
2022-04-02 12:01:46 +0300

    HW8 first part

---
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -0,0 +1,40 @@
+const mealAjaxUrl = "profile/meals/";
+
+const ctx = {
+    ajaxUrl: mealAjaxUrl,
+};
+
+
+$(function () {
+    makeEditable(
+        $("#datatable").DataTable({
+            "paging": false,
+            "info": true,
+            "columns": [
+                {
+                    "data": "description"
+                },
+                {
+                    "data": "datetime"
+                },
+                {
+                    "data": "calories"
+                },
+                {
+                    "defaultContent": "Edit",
+                    "orderable": false
+                },
+                {
+                    "defaultContent": "Delete",
+                    "orderable": false
+                }
+            ],
+            "order": [
+                [
+                    0,
+                    "asc"
+                ]
+            ]
+        })
+    );
+});
